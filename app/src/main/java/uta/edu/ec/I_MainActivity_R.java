b44cb79bfb42d81cf20dda853def6cc44b7a33eb
JIbarra1117 <105680706+JIbarra1117@users.noreply.github.com>
2022-05-30 18:00:45 -0500

    Clase del vector integrada

--- app/src/main/java/uta/edu/ec/I_MainActivity_R.java
+++ app/src/main/java/uta/edu/ec/I_MainActivity_R.java
@@ -1,14 +1,16 @@
 package uta.edu.ec;
 
 import androidx.appcompat.app.AppCompatActivity;
+import uta.edu.ec.I_Vector_R;
+import java.util.ArrayList;
 
 import android.os.Bundle;
 
 public class I_MainActivity_R extends AppCompatActivity {
-
+    ArrayList<I_Vector_R> I_valores_R; // maneja el indice y el valor del vector
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imain_r);
-    }//Hola//Te saludo
+    }
 }
---
+++ app/src/main/java/uta/edu/ec/I_Vector_R.java
@@ -0,0 +1,22 @@
+package uta.edu.ec;
+
+public class I_Vector_R {
+    int I_indice_R;
+    int I_valor_R;
+
+    public int getI_indice_R() {
+        return I_indice_R;
+    }
+
+    public int getI_valor_R() {
+        return I_valor_R;
+    }
+
+    public void setI_indice_R(int i_indice_R) {
+        I_indice_R = i_indice_R;
+    }
+
+    public void setI_valor_R(int i_valor_R) {
+        I_valor_R = i_valor_R;
+    }
+}
