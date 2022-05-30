b15dffa2502caf50664365c0ca1d9960053ccc55
MateoRobalino11 <alexrobalino27@gmail.com>
2022-05-30 17:40:56 -0500

    new

---
+++ app/src/main/java/uta/edu/ec/I_MainActivity_R.java
@@ -0,0 +1,14 @@
+package uta.edu.ec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class I_MainActivity_R extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imain_r);
+    }
+}
