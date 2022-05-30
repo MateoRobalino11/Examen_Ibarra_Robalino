edd493ef7666f1100aa8fdf1f003427476318532
JIbarra1117 <105680706+JIbarra1117@users.noreply.github.com>
2022-05-30 18:35:35 -0500

    18:35

--- app/src/main/java/uta/edu/ec/I_MainActivity_R.java
+++ app/src/main/java/uta/edu/ec/I_MainActivity_R.java
@@ -1,13 +1,14 @@
 package uta.edu.ec;
 
 import androidx.appcompat.app.AppCompatActivity;
-import uta.edu.ec.I_Vector_R;
 import java.util.ArrayList;
 
 import android.os.Bundle;
 
 public class I_MainActivity_R extends AppCompatActivity {
-    ArrayList<I_Vector_R> I_valores_R; // maneja el indice y el valor del vector
+
+    I_Vector_R[] I_valorInicial_R; // maneja el indice y el valor del vector
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/uta/edu/ec/I_Vector_R.java
+++ app/src/main/java/uta/edu/ec/I_Vector_R.java
@@ -19,4 +19,18 @@ public class I_Vector_R {
     public void setI_valor_R(int i_valor_R) {
         I_valor_R = i_valor_R;
     }
+
+    public I_Vector_R[] I_OrdenarVector_R(I_Vector_R[] I_listaValores_R){
+        I_Vector_R[] I_temp_R;
+        for (int i = 0 ; i<I_listaValores_R.length;i++){
+                for (int j = 0 ; j<I_listaValores_R.length;j++){
+                    if(I_listaValores_R[i].I_valor_R>I_temp_R[j].I_valor_R){
+                        I_temp_R =
+
+                    }
+                }
+        }
+        return null;
+    }
 }
+
