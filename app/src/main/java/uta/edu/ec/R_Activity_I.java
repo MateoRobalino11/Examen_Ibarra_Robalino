c124de673d5360fcc614433a423a38a5dd79b3bf
MateoRobalino11 <alexrobalino27@gmail.com>
2022-05-30 20:28:47 -0500

    Cuarto Commit 20:28

--- app/src/main/java/uta/edu/ec/R_Activity_I.java
+++ app/src/main/java/uta/edu/ec/R_Activity_I.java
@@ -2,7 +2,9 @@ package uta.edu.ec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class R_Activity_I extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class R_Activity_I extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ri);
     }
-    //hi
+    public void R_Siguiente_I(View view){
+        Intent R_intent_I = new Intent(this, I_MainActivity_R.class);
+        startActivity(R_intent_I);
+        finish();
+    }
 }
