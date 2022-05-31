056132253bebe7de0a208cb2d03ca0eaad6e7a2a
JIbarra1117 <105680706+JIbarra1117@users.noreply.github.com>
2022-05-30 20:00:12 -0500

    20:00

--- app/src/main/java/uta/edu/ec/I_MainActivity_R.java
+++ app/src/main/java/uta/edu/ec/I_MainActivity_R.java
@@ -1,17 +1,50 @@
 package uta.edu.ec;
 
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class I_MainActivity_R extends AppCompatActivity {
-
-    I_Vector_R[] I_valorInicial_R; // maneja el indice y el valor del vector
-
+    ListView I_lvDatosVector_R;
+    Button I_btnIngresarDatos_R;
+    EditText I_etValor_R;
+    ArrayList<I_Vector_R> I_valoresOriginales_R = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imain_r);
+        I_btnIngresarDatos_R = findViewById(R.id.btnIngresar);
+        I_btnIngresarDatos_R.setOnClickListener((View.OnClickListener) this);
+        I_lvDatosVector_R = findViewById(R.id.ltDatos);
+        I_lvDatosVector_R.setOnClickListener((View.OnClickListener) this);
+
+        I_etValor_R = findViewById(R.id.I_etValor_R);
+    }
+
+    public void IngresarOnClick(View view){
+        /*I_valoresOriginales_R.add(new I_Vector_R(Integer.parseInt(I_etValor_R.getText().toString())));
+        //Convertir A String
+        String[] I_auxString_R = new String[I_valoresOriginales_R.size()];
+        for(int cont = 0; cont< I_valoresOriginales_R.size();cont++){
+            I_auxString_R[cont] = String.valueOf(I_valoresOriginales_R.get(cont).I_valor_R);
+        }
+        I_AdaptarDatas_R(I_auxString_R);*/
+
+        /*switch(){
+
+        }*/
+    }
+
+
+    public void I_AdaptarDatas_R(String[] I_valores_R){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,I_valores_R);
+        I_lvDatosVector_R.setAdapter(adapter);
     }
 }
--- app/src/main/java/uta/edu/ec/I_Vector_R.java
+++ app/src/main/java/uta/edu/ec/I_Vector_R.java
@@ -4,7 +4,7 @@ public class I_Vector_R {
     int I_indice_R;
     int I_valor_R;
 
-    public int getI_indice_R() {
+/*    public int getI_indice_R() {
         return I_indice_R;
     }
 
@@ -19,18 +19,41 @@ public class I_Vector_R {
     public void setI_valor_R(int i_valor_R) {
         I_valor_R = i_valor_R;
     }
+*/
+    public I_Vector_R(int i_valor_R) {
+        I_indice_R =+1;
+        I_valor_R = i_valor_R;
+    }
 
-    public I_Vector_R[] I_OrdenarVector_R(I_Vector_R[] I_listaValores_R){
-        I_Vector_R[] I_temp_R;
+    public int[] I_OrdenarPorIndices_R(I_Vector_R[] I_listaValores_R){
+        int[] I_indices_R = new int[I_listaValores_R.length];
+        int I_temp_R = 0;
+
+        for (int c = 0 ; c<I_listaValores_R.length;c++){
+            if(I_listaValores_R[c].I_valor_R>I_listaValores_R[c].I_valor_R){
+                I_indices_R[c] = I_listaValores_R[c].I_indice_R;
+            }
+        }
         for (int i = 0 ; i<I_listaValores_R.length;i++){
-                for (int j = 0 ; j<I_listaValores_R.length;j++){
-                    if(I_listaValores_R[i].I_valor_R>I_temp_R[j].I_valor_R){
-                        I_temp_R =
-
-                    }
+            for (int j = 0 ; j<I_listaValores_R.length;j++){
+                if(I_listaValores_R[i].I_valor_R>I_listaValores_R[j].I_valor_R){
+                    I_temp_R = I_indices_R[j];
+                    I_indices_R[j]=I_indices_R[i];
+                    I_indices_R[i] = I_temp_R;
                 }
+            }
+        }
+        return I_indices_R;
+    }
+
+    /*public I_Vector_R[] I_OrdenarVector_R(int[][] I_valor_R){
+        I_Vector_R[] I_temp_R;
+        //Ingresar valores de los indices
+
+        for(int i=0; i<I_valor_R.length;i++){
+            I_valor_R[i][1] =
         }
         return null;
-    }
+    }*/
 }
 
